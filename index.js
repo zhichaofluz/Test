02c31b3ad9d56e633d93208bc863ac3908a2c0d2
Zhichao Yang <zhichao@fluzfluz.com>
2018-09-19 13:30:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// The GraphQL schema
+const typeDefs = gql`
+    type Query{
+      "A simple type for getting started!"
+      hello: String
+    }
+`;
+
+//A map of functions which return data fro the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+};
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`server ready at ${url}`)
+});
